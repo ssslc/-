6879e21de9e3a252cccd23f950a8ed62537bce67
liuchao <18174019255@163.com>
2017-07-21 19:25:29 +0800

    Update test.js

--- js/test.js
+++ js/test.js
@@ -1,207 +1,81 @@
+var screenAnimateElements = {
+  '.screen-1':[
+      '.screen-1__heading',
+      '.screen-1__phone',
+      '.screen-1__shadow'
+  ],
+    '.screen-2' :[
+        '.screen-2__heading',
+        '.screen-2__subheading',
+        '.screen-2__phone',
+        '.screen-2__point_i_1',
+        '.screen-2__point_i_2',
+        '.screen-2__point_i_3'
+    ],
+    '.screen-3' :[
+        '.screen-3__heading',
+        '.screen-3__phone',
+        '.screen-3__subheading',
+        '.screen-3__features'
+    ],
+    '.screen-4' :[
+        '.screen-4__heading',
+        '.screen-4__subheading',
+        '.screen-4__type-item_i_1',
+        '.screen-4__type-item_i_2',
+        '.screen-4__type-item_i_3',
+        '.screen-4__type-item_i_4'
+    ],
+    '.screen-5' :[
+        '.screen-5__heading',
+        '.screen-5__subheading',
+        '.screen-5__back'
+    ]
 
 
-(()=>{
-  var test = 
-  [
-  {
-    elem:document.getElementsByClassName('screen-1__heading')[0],
-    init:'screen-1__heading screen-1__heading_animate_init',
-    done:'screen-1__heading screen-1__heading_animate_done'
-  },
-  {
-    elem:document.getElementsByClassName('screen-1__phone')[0],
-    init:'screen-1__phone screen-1__phone_animate_init',
-    done:'screen-1__phone screen-1__phone_animate_done'
-  },
-  {
-    elem:document.getElementsByClassName('screen-1__shadow')[0],
-    init:'screen-1__shadow screen-1__shadow_animate_init',
-    done:'screen-1__shadow screen-1__shadow_animate_done'
-  }
-  ];
-
-  var init = true;
-
-  document.getElementsByClassName('screen-1')[0].onclick =function() {
-    for(var i=0;i<test.length;i++){
-      test[i]['elem'].setAttribute('class', init ? test[i]['init'] : test[i]['done'] );
+};
+function setScreenAnimate(screenCls) {
+    var screen = document.querySelector(screenCls);// get now screen
+    var animateElements = screenAnimateElements[screenCls]; //needed setanielement
+    var isSetAnimateClass =false; //是否初始化子元素样式
+    var isAnimateDone = false;   //当前屏幕下所有子元素的状态是DONE？
+    screen.onclick = function () {
+          //初始化样式，增加init A A_init
+           if( isSetAnimateClass === false ){
+             for (var i=0;i<animateElements.length;i++){
+               var element = document.querySelector(animateElements[i]);
+               var baseCls = element.getAttribute('class');
+               element.setAttribute('class',baseCls + ' ' +animateElements[i].substr(1)+'_animate_init');
+             }
+               isSetAnimateClass = true;
+               return;
+           }
+         //切换所有 animateElements 的 init -> doneA_init  A A_done
+         if(isAnimateDone === false){
+             for (var i=0;i<animateElements.length;i++){
+                 var element = document.querySelector(animateElements[i]);
+                 var baseCls = element.getAttribute('class');
+                 element.setAttribute('class',baseCls.replace('_animate_init','_animate_done') );
+             }
+             isAnimateDone = true;
+             return;
+         }
+        //切换所有 animateElements 的 done -> init   A  A_init
+        if(isAnimateDone === true){
+            for (var i=0;i<animateElements.length;i++){
+                var element = document.querySelector(animateElements[i]);
+                var baseCls = element.getAttribute('class');
+                element.setAttribute('class',baseCls.replace('_animate_done','_animate_init') );
+            }
+            isAnimateDone = false;
+            return;
+        }
     }
-    init = !init;
-  }
-        })(); // 屏幕1测试动画
+}
 
+for(k in screenAnimateElements){
+    setScreenAnimate(k);
+}
 
-        (()=>{
-          var test = 
-          [
-          {
-            elem:document.getElementsByClassName('screen-2__heading')[0],
-            init:'screen-2__heading screen-2__heading_animate_init',
-            done:'screen-2__heading screen-2__heading_animate_done'
-          },
-          {
-            elem:document.getElementsByClassName('screen-2__subheading')[0],
-            init:'screen-2__subheading screen-2__subheading_animate_init',
-            done:'screen-2__subheading screen-2__subheading_animate_done'
-          },
-          {
-            elem:document.getElementsByClassName('screen-2__phone')[0],
-            init:'screen-2__phone screen-2__phone_animate_init',
-            done:'screen-2__phone screen-2__phone_animate_done'
-          },
-          {
-            elem:document.getElementsByClassName('screen-2__point_i_1')[0],
-            init:'screen-2__point screen-2__point_i_1 screen-2__point_i_1_animate_init',
-            done:'screen-2__point screen-2__point_i_1 screen-2__point_i_1_animate_done'
-          },
-          {
-            elem:document.getElementsByClassName('screen-2__point_i_2')[0],
-            init:'screen-2__point screen-2__point_i_2 screen-2__point_animate_init',
-            done:'screen-2__point screen-2__point_i_2 screen-2__point_i_2_animate_done'
-          },
-          {
-            elem:document.getElementsByClassName('screen-2__point_i_3')[0],
-            init:'screen-2__point screen-2__point_i_3 screen-2__point_animate_init',
-            done:'screen-2__point screen-2__point_i_3 screen-2__point_i_3_animate_done'
-          }
-
-          ];
-
-          var init = true;
-
-          document.getElementsByClassName('screen-2')[0].onclick =function() {
-            for(var i=0;i<test.length;i++){
-              test[i]['elem'].setAttribute('class', init ? test[i]['init'] : test[i]['done'] );
-            }
-            init = !init;
-          }
-        })(); // 屏幕2测试动画
-
-
-
-        (()=>{
-          var test = 
-          [
-          {
-            elem:document.getElementsByClassName('screen-3__heading')[0],
-            init:'screen-3__heading screen-3__heading_animate_init',
-            done:'screen-3__heading screen-3__heading_animate_done'
-          },
-          {
-            elem:document.getElementsByClassName('screen-3__subheading')[0],
-            init:'screen-3__subheading screen-3__subheading_animate_init',
-            done:'screen-3__subheading screen-3__subheading_animate_done'
-          },
-          {
-            elem:document.getElementsByClassName('screen-3__phone')[0],
-            init:'screen-3__phone screen-3__phone_animate_init',
-            done:'screen-3__phone screen-3__phone_animate_done'
-          },
-          {
-            elem:document.getElementsByClassName('screen-3__features')[0],
-            init:'screen-3__features screen-3__features_animate_init',
-            done:'screen-3__features screen-3__features_animate_done'
-          }
-
-          ];
-
-          var init = true;
-
-          document.getElementsByClassName('screen-3')[0].onclick =function() {
-            for(var i=0;i<test.length;i++){
-              test[i]['elem'].setAttribute('class', init ? test[i]['init'] : test[i]['done'] );
-            }
-            init = !init;
-          }
-        })(); // 屏幕3测试动画
-
-
-
-        (()=>{
-          var test = 
-          [
-          {
-            elem:document.getElementsByClassName('screen-4__heading')[0],
-            init:'screen-4__heading screen-4__heading_animate_init',
-            done:'screen-4__heading screen-4__heading_animate_done'
-          },
-          {
-            elem:document.getElementsByClassName('screen-4__subheading')[0],
-            init:'screen-4__subheading screen-4__subheading_animate_init',
-            done:'screen-4__subheading screen-4__subheading_animate_done'
-          }
-          ,
-          {
-            elem:document.getElementsByClassName('screen-4__type')[0],
-            init:'screen-4__type screen-4__type_animate_init',
-            done:'screen-4__type screen-4__type_animate_done'
-          }
-
-          ];
-
-          var init = true;
-
-          document.getElementsByClassName('screen-4')[0].onclick =function() {
-            for(var i=0;i<test.length;i++){
-              test[i]['elem'].setAttribute('class', init ? test[i]['init'] : test[i]['done'] );
-            }
-            init = !init;
-          }
-        })(); // 屏幕4测试动画
-
-
-        (()=>{
-          var test = 
-          [
-          {
-            elem:document.getElementsByClassName('screen-5__heading')[0],
-            init:'screen-5__heading screen-5__heading_animate_init',
-            done:'screen-5__heading screen-5__heading_animate_done'
-          },
-          {
-            elem:document.getElementsByClassName('screen-5__subheading')[0],
-            init:'screen-5__subheading screen-5__subheading_animate_init',
-            done:'screen-5__subheading screen-5__subheading_animate_done'
-          }
-          ,
-          {
-            elem:document.getElementsByClassName('screen-5__back')[0],
-            init:'screen-5__back screen-5__back_animate_init',
-            done:'screen-5__back screen-5__back_animate_done'
-          }
-
-          ];
-
-          var init = true;
-
-          document.getElementsByClassName('screen-5')[0].onclick =function() {
-            for(var i=0;i<test.length;i++){
-              test[i]['elem'].setAttribute('class', init ? test[i]['init'] : test[i]['done'] );
-            }
-            init = !init;
-          }
-        })(); // 屏幕5测试动画
-
-
-
-        (()=>{
-          var test = 
-          [
-            'header header_status_black',
-            'header header_status_black header_active_2',
-            'header header_status_black header_active_3',
-            'header header_status_black header_active_4',
-            'header header_status_black header_active_5',
-            'header header_status_black header_active_1',
-            'header',
-          ];
-
-          var idx = -1;
-          document.getElementsByClassName('header')[0].onclick =function() {
-            console.log(idx,'idx >= test.length',idx >= test.length,'next',idx >= test.length ? 0 : idx+1);
-            idx = idx >= test.length-1 ? 0 : idx+1;
-            var cls = test[idx];
-            this.setAttribute('class',cls);
-
-          }
-        })(); // 菜单栏动画切换
+// setScreenAnimate('.screen-1');
+// setScreenAnimate('.screen-2');
